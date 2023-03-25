fb448225cc1698bed314a0f4f895d0ef0079a2e6
Yumi <a13158y@gmail.com>
2023-03-24 18:07:59 -0700

    create user data

--- script.ts
+++ script.ts
@@ -1,3 +1,18 @@
 // This code allows me to access client
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+
+const main = async() => {
+    // this prisma.user came from the User model I created
+    const user = await prisma.user.create({data: {name: "Kyle"}})
+    //user.findMany -> will show all user I created
+    console.log("user", user)
+}
+
+main().catch(e => {
+    console.error(e.message)
+}).finally(async () => {
+    // it is not a must to disconnect
+    await prisma.$disconnect();
+})
